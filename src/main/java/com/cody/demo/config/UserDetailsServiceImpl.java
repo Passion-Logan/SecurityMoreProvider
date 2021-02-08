65ae7319cff7d2b3be385bf7f236ec5dea17ff29
Passion-Logan <wangdadamailbox@163.com>
2021-02-08 23:44:31 +0800

    update

--- src/main/java/com/cody/demo/config/UserDetailsServiceImpl.java
+++ src/main/java/com/cody/demo/config/UserDetailsServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-public class PcUserDetailsServiceImpl implements UserDetailsService {
+public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
     private PcUserDao pcUserDao;
